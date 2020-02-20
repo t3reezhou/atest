15b4e4fa1bcd3fd2d1c46ea72df26d1ac2f8026a
t3ree zhou <t3ree@t3reedeMacBook-Air.local>
2020-02-20 17:43:42 +0800

    tmp3

--- a.go
+++ a.go
@@ -7,12 +7,12 @@ type X int
 var XX []X
 
 func init() {
-	fmt.Println(">>>>>>>", &XX)
+	fmt.Printf(">>>>>>>%p\n", &XX)
 	XX = append(XX, 1000)
 }
 
 func Regist(x X) {
-	fmt.Printf("<<<<<<<<<<<", &XX)
+	fmt.Printf("<<<<<<<<<<<%p\n", &XX)
 	XX = append(XX, x)
 	fmt.Println(">>>>>>>regist", x)
 }
